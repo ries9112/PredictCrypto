3311f0467bec35c69cede18725e8ee58a1ddd964
ries9112 <ries9112@colorado.edu>
2020-02-03 23:24:22 -0700

    Finally got calculate_perc_change() to work as expected

--- R/calculate_perc_change.R
+++ R/calculate_perc_change.R
@@ -1,40 +1,45 @@
 #'@importFrom anytime anytime
 #'@importFrom lubridate hours
+#'@importFrom dplyr left_join
+#'@importFrom dplyr select
+#'@importFrom dplyr rename
+#'@importFrom dplyr filter
 #'@export
-calculate_perc_change <- function(dfHLater, enterHours){
-  # FIX THIS, ISSUE WHEN SUBTRACTING HOURS, NOT WORKING NOW???
+calculate_perc_change <- function (df, enterHours)
+{
+  dfHLater <- cryptoData
 
-  dfHLater <- as.data.frame(dfHLater)
-  #dfHLater$DateTimeColoradoMST <- anytime::anytime(dfHLater$DateTimeColoradoMST)
-  #exclude most recent 12 hours since they wouldn't have data
-  #df12hLater_new <- filter(dfHLater, DateTimeColoradoMST <= max(df$DateTimeColoradoMST) - hours(12) )
-  dfHLater$DateTimeColoradoMST <- dfHLater$DateTimeColoradoMST - (enterHours * 60 * 60)
-  #Replace pkDummy
-  dfHLater$pkDummy <-substr(dfHLater$DateTimeColoradoMST, 1, 13)
-  df$pkDummy <-substr(df$DateTimeColoradoMST, 1, 13)
-  #Create both pkeys
-  df$pkey <- paste0(df$pkDummy,df$Name)
-  dfHLater$pkey <- paste0(dfHLater$pkDummy,dfHLater$Name)
-  #Re-adjust the 12hLater time
-  dfHLater$DateTimeColoradoMST <- dfHLater$DateTimeColoradoMST + hours(enterHours)
-  #narrow down new dataframe to just the Price
-  dfHLater <- select(dfHLater, PriceUSD, pkey, DateTimeColoradoMST) %>% rename(PriceUSD_XhoursLater = PriceUSD, DateTimeColoradoMST_XhoursLater = DateTimeColoradoMST)
-  #join data
-  joinedDataset <- left_join(df, dfHLater, by='pkey')
-  joinedDataset <- filter(joinedDataset, DateTimeColoradoMST <= max(df$DateTimeColoradoMST) - hours(enterHours) )
-  #Now calculate % change
-  joinedDataset$TargetPercChange <- ((joinedDataset$PriceUSD_XhoursLater-joinedDataset$PriceUSD) / joinedDataset$PriceUSD) * 100
-  #Remove first column of the data "X"
-  joinedDataset <- select(joinedDataset,-1)
+  #create 24h offset
+  dfHLater$DateTimeColoradoTimeMST <- dfHLater$DateTimeColoradoTimeMST - lubridate::hours(enterHours)
 
-  #colnames(joinedDataset)[colnames(joinedDataset)=="PriceUSD_XhoursLater"] <- paste0('PriceUSD_',enterHours,'_hours_later')
+  #replace pkDummy
+  dfHLater$pkDummy <- substr(paste(as.POSIXct(dfHLater$DateTimeColoradoTimeMST,format="%Y-%m-%d"), format(as.POSIXct(dfHLater$DateTimeColoradoTimeMST,format="%H:%M:%S"),"%H")),1,13)
 
+  cryptoData$pkDummy <- substr(paste(as.POSIXct(cryptoData$DateTimeColoradoTimeMST,format="%Y-%m-%d"), format(as.POSIXct(cryptoData$DateTimeColoradoTimeMST,format="%H:%M:%S"),"%H")),1,13)
+
+  cryptoData$pkey <- paste(cryptoData$pkDummy, cryptoData$Name)
+
+  dfHLater$pkey <- paste(dfHLater$pkDummy, dfHLater$Name)
+
+  #re-adjust offset
+  dfHLater$DateTimeColoradoTimeMST <- dfHLater$DateTimeColoradoTimeMST + lubridate::hours(enterHours)
+
+  dfHLater <- dplyr::select(dfHLater, PriceUSD, pkey, DateTimeColoradoTimeMST) %>%
+    dplyr::rename(PriceUSD_x_hoursLater = PriceUSD, DateTimeColoradoTimeMST_x_hoursLater = DateTimeColoradoTimeMST)
+
+  joinedDataset <- dplyr::left_join(cryptoData, dfHLater, by = "pkey")
+  #joinedDataset <- filter(joinedDataset, joinedDataset$DateTimeColoradoTimeMST <=
+  #                          max(cryptoData$DateTimeColoradoTimeMST) - (24*60*60 )
+
+  joinedDataset$TargetPercChange <- ((joinedDataset$PriceUSD_x_hoursLater -
+                                        joinedDataset$PriceUSD)/joinedDataset$PriceUSD) * 100
+
+  joinedDataset <- dplyr::select(joinedDataset, -1)
+
+  joinedDataset <<- joinedDataset %>% dplyr::filter(!is.na(TargetPercChange))
   return(joinedDataset)
 }
 
 #### IMPORTANT NOTE FOR CODE ABOVE. RATHER THAN HAVING "XhoursLater", find a way to concat the string of the field name with the user input enterHours! Important, do it before tutorial is too far along!
-
-
-
 
 # remember to create a function just like this but pre-made for a 24 hour period called calculate_24hour_perc_change()
