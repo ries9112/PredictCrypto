e6bbb7abb091bdd7374541d341e87efde3464a75
ries9112 <ries9112@colorado.edu>
2020-02-04 08:28:23 -0700

    Adjust for other df's not called 'df'

--- R/calculate_perc_change.R
+++ R/calculate_perc_change.R
@@ -5,39 +5,39 @@
 #'@importFrom dplyr rename
 #'@importFrom dplyr filter
 #'@export
-calculate_perc_change <- function (df, enterHours)
+calculate_perc_change <- function (crypto_dataset, enterHours)
 {
-  dfHLater <- df
+  crypto_datasetHLater <- crypto_dataset
 
   #create 24h offset
-  dfHLater$DateTimeColoradoTimeMST <- dfHLater$DateTimeColoradoTimeMST - lubridate::hours(enterHours)
+  crypto_datasetHLater$DateTimeColoradoTimeMST <- crypto_datasetHLater$DateTimeColoradoTimeMST - lubridate::hours(enterHours)
 
   #replace pkDummy
-  dfHLater$pkDummy <- substr(paste(as.POSIXct(dfHLater$DateTimeColoradoTimeMST,format="%Y-%m-%d"), format(as.POSIXct(dfHLater$DateTimeColoradoTimeMST,format="%H:%M:%S"),"%H")),1,13)
+  crypto_datasetHLater$pkDummy <- substr(paste(as.POSIXct(crypto_datasetHLater$DateTimeColoradoTimeMST,format="%Y-%m-%d"), format(as.POSIXct(crypto_datasetHLater$DateTimeColoradoTimeMST,format="%H:%M:%S"),"%H")),1,13)
 
-  df$pkDummy <- substr(paste(as.POSIXct(df$DateTimeColoradoTimeMST,format="%Y-%m-%d"), format(as.POSIXct(df$DateTimeColoradoTimeMST,format="%H:%M:%S"),"%H")),1,13)
+  crypto_dataset$pkDummy <- substr(paste(as.POSIXct(crypto_dataset$DateTimeColoradoTimeMST,format="%Y-%m-%d"), format(as.POSIXct(crypto_dataset$DateTimeColoradoTimeMST,format="%H:%M:%S"),"%H")),1,13)
 
-  df$pkey <- paste(df$pkDummy, df$Name)
+  crypto_dataset$pkey <- paste(crypto_dataset$pkDummy, crypto_dataset$Name)
 
-  dfHLater$pkey <- paste(dfHLater$pkDummy, dfHLater$Name)
+  crypto_datasetHLater$pkey <- paste(crypto_datasetHLater$pkDummy, crypto_datasetHLater$Name)
 
   #re-adjust offset
-  dfHLater$DateTimeColoradoTimeMST <- dfHLater$DateTimeColoradoTimeMST + lubridate::hours(enterHours)
+  crypto_datasetHLater$DateTimeColoradoTimeMST <- crypto_datasetHLater$DateTimeColoradoTimeMST + lubridate::hours(enterHours)
 
-  dfHLater <- dplyr::select(dfHLater, PriceUSD, pkey, DateTimeColoradoTimeMST) %>%
+  crypto_datasetHLater <- dplyr::select(crypto_datasetHLater, PriceUSD, pkey, DateTimeColoradoTimeMST) %>%
     dplyr::rename(PriceUSD_x_hoursLater = PriceUSD, DateTimeColoradoTimeMST_x_hoursLater = DateTimeColoradoTimeMST)
 
-  joinedDataset <- dplyr::left_join(df, dfHLater, by = "pkey")
+  joinedDataset <- dplyr::left_join(crypto_dataset, crypto_datasetHLater, by = "pkey")
   #joinedDataset <- filter(joinedDataset, joinedDataset$DateTimeColoradoTimeMST <=
-  #                          max(df$DateTimeColoradoTimeMST) - (24*60*60 )
+  #                          max(crypto_dataset$DateTimeColoradoTimeMST) - (24*60*60 )
 
   joinedDataset$TargetPercChange <- ((joinedDataset$PriceUSD_x_hoursLater -
                                         joinedDataset$PriceUSD)/joinedDataset$PriceUSD) * 100
 
   joinedDataset <- dplyr::select(joinedDataset, -1)
 
-  df <<- joinedDataset %>% dplyr::filter(!is.na(TargetPercChange))
-  #return(df)
+  crypto_dataset <<- joinedDataset %>% dplyr::filter(!is.na(TargetPercChange))
+  #return(crypto_dataset)
 }
 
 #### IMPORTANT NOTE FOR CODE ABOVE. RATHER THAN HAVING "XhoursLater", find a way to concat the string of the field name with the user input enterHours! Important, do it before tutorial is too far along!
