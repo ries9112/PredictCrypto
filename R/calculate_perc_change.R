c0b263173f4d907008305157a36a26d2df087747
ries9112 <ries9112@colorado.edu>
2020-02-04 08:23:59 -0700

    Make calculate_perc_change() work on Shrimpy

--- R/calculate_perc_change.R
+++ R/calculate_perc_change.R
@@ -7,7 +7,7 @@
 #'@export
 calculate_perc_change <- function (df, enterHours)
 {
-  dfHLater <- cryptoData
+  dfHLater <- df
 
   #create 24h offset
   dfHLater$DateTimeColoradoTimeMST <- dfHLater$DateTimeColoradoTimeMST - lubridate::hours(enterHours)
@@ -15,9 +15,9 @@ calculate_perc_change <- function (df, enterHours)
   #replace pkDummy
   dfHLater$pkDummy <- substr(paste(as.POSIXct(dfHLater$DateTimeColoradoTimeMST,format="%Y-%m-%d"), format(as.POSIXct(dfHLater$DateTimeColoradoTimeMST,format="%H:%M:%S"),"%H")),1,13)
 
-  cryptoData$pkDummy <- substr(paste(as.POSIXct(cryptoData$DateTimeColoradoTimeMST,format="%Y-%m-%d"), format(as.POSIXct(cryptoData$DateTimeColoradoTimeMST,format="%H:%M:%S"),"%H")),1,13)
+  df$pkDummy <- substr(paste(as.POSIXct(df$DateTimeColoradoTimeMST,format="%Y-%m-%d"), format(as.POSIXct(df$DateTimeColoradoTimeMST,format="%H:%M:%S"),"%H")),1,13)
 
-  cryptoData$pkey <- paste(cryptoData$pkDummy, cryptoData$Name)
+  df$pkey <- paste(df$pkDummy, df$Name)
 
   dfHLater$pkey <- paste(dfHLater$pkDummy, dfHLater$Name)
 
@@ -27,17 +27,17 @@ calculate_perc_change <- function (df, enterHours)
   dfHLater <- dplyr::select(dfHLater, PriceUSD, pkey, DateTimeColoradoTimeMST) %>%
     dplyr::rename(PriceUSD_x_hoursLater = PriceUSD, DateTimeColoradoTimeMST_x_hoursLater = DateTimeColoradoTimeMST)
 
-  joinedDataset <- dplyr::left_join(cryptoData, dfHLater, by = "pkey")
+  joinedDataset <- dplyr::left_join(df, dfHLater, by = "pkey")
   #joinedDataset <- filter(joinedDataset, joinedDataset$DateTimeColoradoTimeMST <=
-  #                          max(cryptoData$DateTimeColoradoTimeMST) - (24*60*60 )
+  #                          max(df$DateTimeColoradoTimeMST) - (24*60*60 )
 
   joinedDataset$TargetPercChange <- ((joinedDataset$PriceUSD_x_hoursLater -
                                         joinedDataset$PriceUSD)/joinedDataset$PriceUSD) * 100
 
   joinedDataset <- dplyr::select(joinedDataset, -1)
 
-  cryptoData <<- joinedDataset %>% dplyr::filter(!is.na(TargetPercChange))
-  #return(cryptoData)
+  df <<- joinedDataset %>% dplyr::filter(!is.na(TargetPercChange))
+  #return(df)
 }
 
 #### IMPORTANT NOTE FOR CODE ABOVE. RATHER THAN HAVING "XhoursLater", find a way to concat the string of the field name with the user input enterHours! Important, do it before tutorial is too far along!
