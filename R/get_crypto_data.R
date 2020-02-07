8c25aab3f7f40b8819b72b088f25dd904015e256
Riccardo <ries9112@colorado.edu>
2020-02-06 23:00:44 -0700

    github actions test

--- R/get_crypto_data.R
+++ R/get_crypto_data.R
@@ -3,7 +3,7 @@
 #'@importFrom safer decrypt_string
 #'@export
 get_crypto_data <- function(){
-  pins::board_register_github(name = "github", repo = "ries9112/Pins", branch = "master", token = decrypt_string("+MKmN4M7IPrv/ozonR1SpkTSj/gKyPOcECnnAm9v1YhNGjOk3nf8eiACiZWckdKYLVEJV3eNlew=", '123'))
+  pins::board_register_github(name = "github", repo = "ries9112/Pins", branch = "master", token = decrypt_string("+MKmN4M7IPrv/ozonR1SpkTSj/gKyPOcECnnAm9v1YhNGjOk3nf8eiACiZWckdKYLVEJV3eNlew=", toString(20.5*6)))
 
   # Pull data from board
   cryptoData <<- pin_get('cryptoData','github') # double << lets it be part of global environment from function
