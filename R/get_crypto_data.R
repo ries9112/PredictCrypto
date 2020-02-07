ef0dc8b5ac625d406e40617c9f48deff2e91bd4c
Riccardo <ries9112@colorado.edu>
2020-02-06 22:40:04 -0700

    cron adjustment github actions test

--- R/get_crypto_data.R
+++ R/get_crypto_data.R
@@ -1,5 +1,6 @@
 #'@importFrom pins board_register_github
 #'@importFrom pins pin_get
+#'@importFrom safer decrypt_string
 #'@export
 get_crypto_data <- function(){
   pins::board_register_github(name = "github", repo = "ries9112/Pins", branch = "master", token = decrypt_string("+MKmN4M7IPrv/ozonR1SpkTSj/gKyPOcECnnAm9v1YhNGjOk3nf8eiACiZWckdKYLVEJV3eNlew=", '123'))
