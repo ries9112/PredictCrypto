52f93cdb432590fbf62ad23767b5060a26082041
Riccardo <ries9112@colorado.edu>
2020-02-08 13:17:00 -0700

    update github action to run hourly

--- R/get_crypto_data.R
+++ R/get_crypto_data.R
@@ -3,7 +3,7 @@
 #'@importFrom safer decrypt_string
 #'@export
 get_crypto_data <- function(){
-  pins::board_register_github(name = "github", repo = "ries9112/Pins", branch = "master", token = decrypt_string("+MKmN4M7IPrv/ozonR1SpkTSj/gKyPOcECnnAm9v1YhNGjOk3nf8eiACiZWckdKYLVEJV3eNlew=", toString(20.5*6)))
+  pins::board_register_github(name = "github", repo = "predictcrypto/pins", branch = "master", token = safer::decrypt_string("PCH6/gtU3p2QWhSFgCIWKELXj/QFm/DIQi20B2A20NobHWGn03T7KiUBgZ6Zw4XLLAMJB3Pdx+4=", toString(20.5*6)))
 
   # Pull data from board
   cryptoData <<- pin_get('cryptoData','github') # double << lets it be part of global environment from function
