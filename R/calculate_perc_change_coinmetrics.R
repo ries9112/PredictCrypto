e465daff9e3fc4a236374ab698b69aabbcf7c40c
Riccardo <ries9112@colorado.edu>
2020-02-12 16:21:14 -0700

    converted variable to snake_case

--- R/calculate_perc_change_coinmetrics.R
+++ R/calculate_perc_change_coinmetrics.R
@@ -27,14 +27,14 @@ calculate_perc_change_coinmetrics <- function (coin_metrics, enter_hours)
   coin_metricsHLater$date_time_utc <- coin_metricsHLater$date_time_utc + lubridate::hours(enter_hours)
 
   coin_metricsHLater <- dplyr::select(coin_metricsHLater, price_usd, pkey, date_time_utc) %>%
-    #dplyr::rename("price_usd_{{ enter_hours }}_hoursLater" = price_usd, date_time_utc_x_hoursLater = date_time_utc)
-    dplyr::rename(price_usd_x_hoursLater = price_usd, date_time_utc_x_hoursLater = date_time_utc)
+    #dplyr::rename("price_usd_{{ enter_hours }}_hoursLater" = price_usd, date_time_utc_x_hours_later = date_time_utc)
+    dplyr::rename(price_usd_x_hours_later = price_usd, date_time_utc_x_hours_later = date_time_utc)
 
   joinedDataset <- dplyr::left_join(coin_metrics, coin_metricsHLater, by = "pkey")
   #joinedDataset <- filter(joinedDataset, joinedDataset$date_time_utc <=
   #                          max(coin_metrics$date_time_utc) - (24*60*60 )
 
-  joinedDataset$TargetPercChange <- ((as.numeric(joinedDataset$price_usd_x_hoursLater) -
+  joinedDataset$TargetPercChange <- ((as.numeric(joinedDataset$price_usd_x_hours_later) -
                                         as.numeric(joinedDataset$price_usd))/as.numeric(joinedDataset$price_usd)) * 100
 
   joinedDataset <- dplyr::select(joinedDataset, -1)
