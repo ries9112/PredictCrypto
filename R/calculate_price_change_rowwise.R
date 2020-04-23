e8b2245f70129d9f5268f990d2cb5efb52b7f0d1
Riccardo Esclapon <ries9112@colorado.edu>
2020-04-23 10:39:56 -0600

    having calculate_price_change_rowwise() reference calculate_price_change() caused package install to break so I removed it

--- R/calculate_price_change_rowwise.R
+++ R/calculate_price_change_rowwise.R
@@ -1,8 +1,10 @@
-#'@importFrom PredictCrypto calculate_price_change
-#'@export
-calculate_price_change_rowwise <- function(crypto_data, hours){ #would be better to take name/unique identifier from user too here
-  for (i in length(crypto_data)){
-    crypto_data$data[[i]] <- PredictCrypto::calculate_price_change(crypto_data$data[[i]], hours)
-  }
-  return(crypto_data)
-}
+#@importFrom PredictCrypto calculate_price_change
+#@export
+
+# DOES NOT WORK (MAKES PACKAGE INSTALL BREAK!). Removed roxygen syntax to comment out.
+# calculate_price_change_rowwise <- function(crypto_data, hours){ #would be better to take name/unique identifier from user too here
+#   for (i in length(crypto_data)){
+#     crypto_data$data[[i]] <- PredictCrypto::calculate_price_change(crypto_data$data[[i]], hours)
+#   }
+#   return(crypto_data)
+# }
