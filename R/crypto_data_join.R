d5834125fa4c66064cd218de06b5a0229cbb6a55
Riccardo Esclapon <ries9112@colorado.edu>
2020-04-29 12:20:12 -0600

    add exchange back into result

--- R/crypto_data_join.R
+++ R/crypto_data_join.R
@@ -12,9 +12,9 @@ crypto_data_join <- function(full_dataset, data_with_prices){
   # add pkey to data_with_prices
   data_with_prices$pkey <- paste0(data_with_prices$pkDummy, data_with_prices$Name)
   # merge the two
-  crypto_data <- merge(x = full_dataset, y = data_with_prices[, c("pkey","exchange_price")], by = "pkey", all.x = T)
+  crypto_data <- merge(x = full_dataset, y = data_with_prices[, c("pkey","exchange_price", "Exchange")], by = "pkey", all.x = T)
   # make tweaks to the result
-  crypto_data <- crypto_data %>% select(-price_usd) %>% rename(price_usd = "exchange_price") %>% subset(price_usd > 0)
+  crypto_data <- crypto_data %>% select(-price_usd) %>% rename(price_usd = "exchange_price", exchange='Exchange') %>% subset(price_usd > 0)
   # return crypto_data
   return(crypto_data)
   # remove empty objects and label these as being from the exchange full_dataset:
