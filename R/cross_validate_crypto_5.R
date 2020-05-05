447c435f75f28d1c3314190dedde28e658d3654c
Riccardo Esclapon <ries9112@colorado.edu>
2020-05-04 19:29:20 -0600

    trying to get cross validation the way I want it

--- R/cross_validate_crypto_5.R
+++ R/cross_validate_crypto_5.R
@@ -34,9 +34,9 @@ cross_validate_crypto_5 <- function (data) {
     #     # initialize training column
     #     fold_data$data[[i]]$training <-
     #     # add training labels
-    #     fold_data$data[[i]][fold_data$data[[i]]$date_time_colorado_mst < toString(quantile(fold_data$data[[i]]$date_time_colorado_mst, 0.7)),]$training <- 'train'
+    #     fold_data$data[[i]][fold_data$data[[i]]$date_time_colorado_mst < toString(quantile(fold_data$data[[i]]$date_time_colorado_mst, 0.8)),]$training <- 'train'
     #     # same for test
-    #     fold_data$data[[i]][fold_data$data[[i]]$date_time_colorado_mst >= toString(quantile(fold_data$data[[i]]$date_time_colorado_mst, 0.7)),]$training <- 'test'
+    #     fold_data$data[[i]][fold_data$data[[i]]$date_time_colorado_mst >= toString(quantile(fold_data$data[[i]]$date_time_colorado_mst, 0.8)),]$training <- 'test'
     #     # Remove the 30% that went to test from the total data
     #     all_data$data[[i]] <- all_data$data[[i]][all_data$data[[i]]$date_time_colorado_mst < min(all_data$data[[i]]$date_time_colorado_mst),]
     #
@@ -53,28 +53,28 @@ cross_validate_crypto_5 <- function (data) {
 
     # One at a time:
     #print(i)
-    # Use % (50%) to take most recent % of all data
+    # Take most recent 30% of data (then 40%, 50%, 60%, 70%)
     all_data <- data$data[[i]]
-    fold1_data <- all_data[all_data$date_time_colorado_mst >= toString(quantile(all_data$date_time_colorado_mst, folds_percentile)),]
-    # Create train/test split 70/30 for current data
-    fold1_data_train <- fold1_data[fold1_data$date_time_colorado_mst < toString(quantile(fold1_data$date_time_colorado_mst, 0.7)),]
+    fold1_data <- all_data[all_data$date_time_colorado_mst >= toString(quantile(all_data$date_time_colorado_mst, 0.3)),]
+    # Create train/test split 80/20 for current data
+    fold1_data_train <- fold1_data[fold1_data$date_time_colorado_mst < toString(quantile(fold1_data$date_time_colorado_mst, 0.8)),]
     fold1_data_train$training <- 'train'
     fold1_data_train$split <- 1
     # same for test
-    fold1_data_test <- fold1_data[fold1_data$date_time_colorado_mst >= toString(quantile(fold1_data$date_time_colorado_mst, 0.7)),]
+    fold1_data_test <- fold1_data[fold1_data$date_time_colorado_mst >= toString(quantile(fold1_data$date_time_colorado_mst, 0.8)),]
     fold1_data_test$training <- 'holdout'
     fold1_data_test$split <- 1
     # Remove the 30% that went to test from the total data
     all_data <- complete_data$data[[i]][complete_data$data[[i]]$date_time_colorado_mst < min(fold1_data_test$date_time_colorado_mst),]
     print(i)
-    # Take new most recent 50% of all data
-    fold2_data <- all_data[all_data$date_time_colorado_mst >= toString(quantile(all_data$date_time_colorado_mst, folds_percentile)),]
-    # Create train/test split 70/30 for current data
-    fold2_data_train <- fold2_data[fold2_data$date_time_colorado_mst < toString(quantile(fold2_data$date_time_colorado_mst, 0.7)),]
+    # Take new most recent 40% of all data
+    fold2_data <- all_data[all_data$date_time_colorado_mst >= toString(quantile(all_data$date_time_colorado_mst, 0.4)),]
+    # Create train/test split 80/20 for current data
+    fold2_data_train <- fold2_data[fold2_data$date_time_colorado_mst < toString(quantile(fold2_data$date_time_colorado_mst, 0.8)),]
     fold2_data_train$training <- 'train'
     fold2_data_train$split <- 2
     # same for test
-    fold2_data_test <- fold2_data[fold2_data$date_time_colorado_mst >= toString(quantile(fold2_data$date_time_colorado_mst, 0.7)),]
+    fold2_data_test <- fold2_data[fold2_data$date_time_colorado_mst >= toString(quantile(fold2_data$date_time_colorado_mst, 0.8)),]
     fold2_data_test$training <- 'test'
     fold2_data_test$split <- 2
     # Remove the 30% that went to test from the total data
@@ -82,40 +82,40 @@ cross_validate_crypto_5 <- function (data) {
     # third fold:
     # Take new most recent 50% of all data
     fold3_data <- all_data[all_data$date_time_colorado_mst >= toString(quantile(all_data$date_time_colorado_mst, folds_percentile)),]
-    # Create train/test split 70/30 for current data
-    fold3_data_train <- fold3_data[fold3_data$date_time_colorado_mst < toString(quantile(fold3_data$date_time_colorado_mst, 0.7)),]
+    # Create train/test split 80/20 for current data
+    fold3_data_train <- fold3_data[fold3_data$date_time_colorado_mst < toString(quantile(fold3_data$date_time_colorado_mst, 0.8)),]
     fold3_data_train$training <- 'train'
     fold3_data_train$split <- 3
     # same for test
-    fold3_data_test <- fold3_data[fold3_data$date_time_colorado_mst >= toString(quantile(fold3_data$date_time_colorado_mst, 0.7)),]
+    fold3_data_test <- fold3_data[fold3_data$date_time_colorado_mst >= toString(quantile(fold3_data$date_time_colorado_mst, 0.8)),]
     fold3_data_test$training <- 'test'
     fold3_data_test$split <- 3
     # Remove the 30% that went to test from the total data
     all_data <- complete_data$data[[i]][complete_data$data[[i]]$date_time_colorado_mst < min(fold3_data_test$date_time_colorado_mst),]
 
     # fourth fold:
-    # Take new most recent 50% of all data
-    fold4_data <- all_data[all_data$date_time_colorado_mst >= toString(quantile(all_data$date_time_colorado_mst, folds_percentile)),]
-    # Create train/test split 70/30 for current data
-    fold4_data_train <- fold4_data[fold4_data$date_time_colorado_mst < toString(quantile(fold4_data$date_time_colorado_mst, 0.7)),]
+    # Take new most recent 60% of all data
+    fold4_data <- all_data[all_data$date_time_colorado_mst >= toString(quantile(all_data$date_time_colorado_mst, 0.6)),]
+    # Create train/test split 80/20 for current data
+    fold4_data_train <- fold4_data[fold4_data$date_time_colorado_mst < toString(quantile(fold4_data$date_time_colorado_mst, 0.8)),]
     fold4_data_train$training <- 'train'
     fold4_data_train$split <- 4
     # same for test
-    fold4_data_test <- fold4_data[fold4_data$date_time_colorado_mst >= toString(quantile(fold4_data$date_time_colorado_mst, 0.7)),]
+    fold4_data_test <- fold4_data[fold4_data$date_time_colorado_mst >= toString(quantile(fold4_data$date_time_colorado_mst, 0.8)),]
     fold4_data_test$training <- 'test'
     fold4_data_test$split <- 4
     # Remove the 30% that went to test from the total data
     all_data <- complete_data$data[[i]][complete_data$data[[i]]$date_time_colorado_mst < min(fold4_data_test$date_time_colorado_mst),]
 
     # Fifth fold:
-    # Take new most recent 50% of all data
-    fold5_data <- all_data[all_data$date_time_colorado_mst >= toString(quantile(all_data$date_time_colorado_mst, folds_percentile)),]
-    # Create train/test split 70/30 for current data
-    fold5_data_train <- fold5_data[fold5_data$date_time_colorado_mst < toString(quantile(fold5_data$date_time_colorado_mst, 0.7)),]
+    # Take new most recent 70% of all data
+    fold5_data <- all_data[all_data$date_time_colorado_mst >= toString(quantile(all_data$date_time_colorado_mst, 0.7)),]
+    # Create train/test split 80/20 for current data
+    fold5_data_train <- fold5_data[fold5_data$date_time_colorado_mst < toString(quantile(fold5_data$date_time_colorado_mst, 0.8)),]
     fold5_data_train$training <- 'train'
     fold5_data_train$split <- 5
     # same for test
-    fold5_data_test <- fold5_data[fold5_data$date_time_colorado_mst >= toString(quantile(fold5_data$date_time_colorado_mst, 0.7)),]
+    fold5_data_test <- fold5_data[fold5_data$date_time_colorado_mst >= toString(quantile(fold5_data$date_time_colorado_mst, 0.8)),]
     fold5_data_test$training <- 'test'
     fold5_data_test$split <- 5
     # Remove the 30% that went to test from the total data
