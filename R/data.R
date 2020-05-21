aea0bcc5ad18d10bf67e1efb7e940fc10ff164ce
ries9112 <info@predictcrypto.net>
2020-05-20 18:39:32 -0600

    adjustment to imports to remove MySQL and added messari_reprex to package and started documentation

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Prices of 50,000 round cut diamonds.
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{DateExtracted}{Date the data was collected on - Colorado Timezone}
+#'   \item{DateTimeColoradoMDT}{Date and Time the data was collected on - Colorado Timezone}
+#'   \item{Name}{The name of the cryptocurrency}
+#'   \item{Rank}{The rank in terms of market capitalization of the cryptocurrency at the time the data was collected}
+#'   \item{PriceUSD}{Price in US Dollars}
+#'   ...
+#' }
+#' @source \url{https://messari.io/}
+"messari_reprex"
