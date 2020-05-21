9a00cf74fa6d7f491e3bdee5a2f206458d067235
ries9112 <info@predictcrypto.net>
2020-05-20 20:16:12 -0600

    document messari_reprex data

--- R/data.R
+++ R/data.R
@@ -1,9 +1,8 @@
 #' Prices of 50,000 round cut diamonds.
 #'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds.
+#' A dataset containing cryptocurrency data from messari.io
 #'
-#' @format A data frame with 53940 rows and 10 variables:
+#' @format A data frame with 1369627 rows and 83 variables:
 #' \describe{
 #'   \item{DateExtracted}{Date the data was collected on - Colorado Timezone}
 #'   \item{DateTimeColoradoMDT}{Date and Time the data was collected on - Colorado Timezone}
