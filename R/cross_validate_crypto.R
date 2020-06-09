aa3c6c72b91ea707adde9f5b6a83dc2dfd35f5a4
Riccardo Esclapon <ries9112@colorado.edu>
2020-06-08 23:27:22 -0600

    new cross_validate_crypto works (for 5 folds)

--- R/cross_validate_crypto.R
+++ R/cross_validate_crypto.R
@@ -1,93 +1,80 @@
 #'@importFrom magrittr %>%
-#'@importFrom dplyr arrange
-#'@importFrom dplyr mutate
+#'@importFrom dplyr dplyr::mutate
 #'@importFrom dplyr ntile
-#'@importFrom dplyr group_by
-#'@importFrom dplyr filter
-#'@importFrom dplyr bind_rows
-#'@export
 cross_validate_crypto <- function(data, splits = 5) {
-  # make new object
-  quantiles <- data
-  # create split
-  for (i in 1:nrow(data)) {
-    quantiles$data[[i]] <-
-      data$data[[i]] %>%
-      arrange(rank) %>%
-      arrange(date_extracted) %>%
-      mutate(split = ntile(date_extracted, splits))
-  }
+  # for the first split, start by taking most recent 60% of the data
+  split_one <- data[dplyr::ntile(data$date_time_colorado_mst, 10) > 4,]
+  # still first split, take 80/20 split for train and test
+  split_one_train <- split_one[dplyr::ntile(split_one$date_time_colorado_mst, 10) <= 8,] %>% dplyr::mutate(training='train')
+  # now test
+  split_one_test <- split_one[dplyr::ntile(split_one$date_time_colorado_mst, 10) > 8,] %>% dplyr::mutate(training='holdout')
+  # union the two to create split_one again
+  split_one <- dplyr::union(split_one_train, split_one_test)
+  # for the remaining 40% of the data, find the number of rows to add to the tail end each time
+  rows_per_split <- (nrow(split_one_train) + nrow(split_one_test)) / (splits+1)
+  # record the current minimum date/time to use it as a starting point for the next split
+  split_one_min_datetime <- min(split_one_train$date_time_colorado_mst)
 
-  # create train data with first 70% of dates
-  quantiles_train <- quantiles
-  for (i in 1:nrow(data)) {
-    quantiles_train$data[[i]] <- quantiles$data[[i]] %>%
-      group_by(split) %>%
-      mutate(training = 'train') %>%
-      filter(date_extracted <= quantile(date_extracted, .7))
-  }
+  # start working on the second split.
+  # start by taking train from first split (which naturally excludes test)
+  split_two <- split_one_train
+  # now add number of rows determined earlier
+  split_two <- union(dplyr::mutate(head(data[data$date_time_colorado_mst < split_one_min_datetime,], rows_per_split), training='train'), split_two)
+  # now make the latest 20% into test
+  split_two_test <- split_two[dplyr::ntile(split_two$date_time_colorado_mst, 10) > 8,] %>% dplyr::mutate(training='test')
+  # remove latest 20% of data
+  split_two <- split_two[dplyr::ntile(split_two$date_time_colorado_mst, 10) <= 8,]
+  # union back in
+  split_two <- dplyr::union(split_two, split_two_test)
 
-  # create test data with last 70% of dates
-  quantiles_test <- quantiles
-  for (i in 1:nrow(data)) {
-    quantiles_test$data[[i]] <- quantiles$data[[i]] %>%
-      group_by(split) %>%
-      mutate(training = 'test') %>%
-      filter(date_extracted > quantile(date_extracted, .7))
+  # now third split
+  split_three <- split_two[dplyr::ntile(split_two$date_time_colorado_mst, 10) <= 8,]
+  # now add number of rows determined earlier
+  split_three <- union(dplyr::mutate(head(data[data$date_time_colorado_mst < min(split_two$date_time_colorado_mst),], rows_per_split), training='train'), split_three)
+  # now make the latest 20% into test
+  split_three_test <- split_three[dplyr::ntile(split_three$date_time_colorado_mst, 10) > 8,] %>% dplyr::mutate(training='test')
+  # remove latest 20% of data
+  split_three <- split_three[dplyr::ntile(split_three$date_time_colorado_mst, 10) <= 8,]
+  # union back in
+  split_three <- dplyr::union(split_three, split_three_test)
 
-    # make the last split the holdout
-    quantiles_test$data[[i]][quantiles_test$data[[i]]$split == splits, 'training'] <-
-      'holdout'
+  # now fourth split
+  split_four <- split_three[dplyr::ntile(split_three$date_time_colorado_mst, 10) <= 8,]
+  # now add number of rows determined earlier
+  split_four <- union(dplyr::mutate(head(data[data$date_time_colorado_mst < min(split_three$date_time_colorado_mst),], rows_per_split), training='train'), split_four)
+  # now make the latest 20% into test
+  split_four_test <- split_four[dplyr::ntile(split_four$date_time_colorado_mst, 10) > 8,] %>% dplyr::mutate(training='test')
+  # remove latest 20% of data
+  split_four <- split_four[dplyr::ntile(split_four$date_time_colorado_mst, 10) <= 8,]
+  # union back in
+  split_four <- dplyr::union(split_four, split_four_test)
 
-  }
-
-  # Don't waste data from test sets, add to next train
-  quantiles_train_new <- data.frame()
-  quantiles_train_temp <- data.frame()
-  for (j in 1:nrow(quantiles_train)) {
-    for (i in 1:splits) {
-      if (j == 1) {
-        # if first iteration need to first initialize new objects _temp and _new
-        quantiles_train_temp <- quantiles_train
-        quantiles_train_new <- quantiles_train_temp
-
-        quantiles_train_temp$data[[j]] <-
-          quantiles_train$data[[j]] %>%
-          filter(split == i) %>%
-          bind_rows(filter(quantiles_test$data[[j]], split == i - 1))
-
-        quantiles_train_new$data[[j]] <-
-          bind_rows(quantiles_train_new$data[[j]],
-                    quantiles_train_temp$data[[j]])
-      }
-      else{
-        quantiles_train_temp$data[[j]] <- quantiles_train$data[[j]] %>%
-          filter(split == i) %>%
-          bind_rows(filter(quantiles_test$data[[j]], split == i - 1))
-
-        quantiles_train_new$data[[j]] <-
-          bind_rows(quantiles_train_new$data[[j]],
-                    quantiles_train_temp$data[[j]])
-      }
-    }
-  }
-  quantiles_train_new <-
-    quantiles_train_new %>% mutate(exchange = 'HitBTC')
+  # now fifth split
+  split_five <- split_four[dplyr::ntile(split_four$date_time_colorado_mst, 10) <= 8,]
+  # now add number of rows determined earlier
+  split_five <- union(dplyr::mutate(head(data[data$date_time_colorado_mst < min(split_four$date_time_colorado_mst),], rows_per_split), training='train'), split_five)
+  # now make the latest 20% into test
+  split_five_test <- split_five[dplyr::ntile(split_five$date_time_colorado_mst, 10) > 8,] %>% dplyr::mutate(training='test')
+  # remove latest 20% of data
+  split_five <- split_five[dplyr::ntile(split_five$date_time_colorado_mst, 10) <= 8,]
+  # union back in
+  split_five <- dplyr::union(split_five, split_five_test)
 
 
-  # Now create new data structure with new columns train and test with 5 splits:
-  quantiles_new <- quantiles_train_new
-  quantiles_grouped <- quantiles_new
-  for (i in 1:nrow(quantiles_train_new)) {
-    quantiles_new$data[[i]] <-
-      bind_rows(quantiles_train_new$data[[i]], quantiles_test$data[[i]])
+  # specify splits as columns
+  split_one$split <- 1
+  split_two$split <- 2
+  split_three$split <- 3
+  split_four$split <- 4
+  split_five$split <- 5
 
-    quantiles_grouped$data[[i]] <-
-      quantiles_new$data[[i]] %>% nest(-split,-training) %>% spread(training, data)
-  }
-
-  # save the un-nested version so the dates can be visualized easier
-  cross_validate_show_dates <<- quantiles_new
-
-  return(quantiles_grouped)
+  # at the end, union all
+  finalized_data <- dplyr::union(split_one, split_two) %>%
+    dplyr::union(split_three) %>%
+    dplyr::union(split_four) %>%
+    dplyr::union(split_five)
 }
+
+#NOTES: STILL HARDCODED FOR 5 FOLDS!
+
+
