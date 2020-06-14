84303dce7448b7217c474bcd846a2a3f3436bedd
Riccardo Esclapon <ries9112@colorado.edu>
2020-06-13 18:10:43 -0600

    adjusted crypto_data_join() for order books data

--- R/crypto_data_join.R
+++ R/crypto_data_join.R
@@ -3,8 +3,9 @@
 #'@importFrom dplyr  rename
 #'@export
 crypto_data_join <- function(full_dataset, data_with_prices){
-  # rename field
-  data_with_prices$exchange_price <- data_with_prices$Price
+  # rename fields
+  data_with_prices$buy_price_low_ask <- data_with_prices$lowest_ask_1_price_usd
+  data_with_prices$sell_price_high_bid <- data_with_prices$highest_bid_1_price_usd
   # initialize crypto_data
   crypto_data <- data_with_prices
   # add pkey to full_dataset
@@ -12,9 +13,10 @@ crypto_data_join <- function(full_dataset, data_with_prices){
   # add pkey to data_with_prices
   data_with_prices$pkey <- paste0(data_with_prices$pkDummy, data_with_prices$Name)
   # merge the two
-  crypto_data <- merge(x = full_dataset, y = data_with_prices[, c("pkey","exchange_price", "Exchange")], by = "pkey", all.x = T)
+  crypto_data <- merge(x = full_dataset, y = data_with_prices[, c("pkey","buy_price_low_ask","sell_price_high_bid", "Exchange")], by = "pkey", all.x = T)
   # make tweaks to the result
-  crypto_data <- crypto_data %>% select(-price_usd) %>% rename(price_usd = "exchange_price", exchange='Exchange') %>% subset(price_usd > 0)
+  crypto_data <- crypto_data %>% subset(buy_price_low_ask > 0)
+  crypto_data <- crypto_data %>% subset(sell_price_high_bid > 0)
   # return crypto_data
   return(crypto_data)
   # remove empty objects and label these as being from the exchange full_dataset:
