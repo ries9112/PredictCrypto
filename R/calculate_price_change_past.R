88622c78a669364cd5f8ade1eb08977b10a17bb9
Riccardo Esclapon <ries9112@colorado.edu>
2020-06-13 22:02:28 -0600

    added function to calculate previous % change

---
+++ R/calculate_price_change_past.R
@@ -0,0 +1,39 @@
+#'@importFrom lubridate hours
+#'@importFrom dplyr rename
+#'@importFrom dplyr distinct
+#'@export
+calculate_price_change_past <- function(data, hours_later){ #would be better to take name/unique identifier from user too here
+
+  # make pkey for join
+  data$pkey <- paste0(data$pk_dummy, data$name)
+
+  # new object for join offset
+  data_join <- data
+
+  # adjust by x hours
+  data_join$date_time_colorado_mst <- data_join$date_time_colorado_mst + lubridate::hours(hours_later)
+
+  # make adjusted pk_dummy
+  data_join$pk_dummy <- substr(data_join$date_time_colorado_mst, 1, 13)
+
+  # make adjusted pkey
+  data_join$pkey <- paste0(data_join$pk_dummy, data_join$name)
+
+  # rename new offset price_usd field (only sell price needed)
+  data_join <- rename(data_join, sell_price_usd_24h_earlier = 'sell_price_high_bid') # ADJUST FIELD NAME WITH TIDY EVAL
+
+  # join data and overwrite old object
+  data_join <- merge(x=data, y=data_join[, c('pkey', 'sell_price_usd_24h_earlier')], by = 'pkey', all.x = T)
+
+  # remove rows without target
+  data_join <- data_join[complete.cases(data_join[,'sell_price_usd_24h_earlier']),]
+
+  # adjust pkey by exchange
+  data_join$pkey <- paste0(data_join$pkey, data_join$exchange)
+
+  # unique data and return the result
+  return(dplyr::distinct(data_join, pkey, .keep_all = T))
+}
+
+# Note 04/21: Remember to create new tests
+
