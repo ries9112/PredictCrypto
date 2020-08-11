0ce3ba4568653edbf5dec744d5f753bafd4fe82f
Riccardo Esclapon <ries9112@colorado.edu>
2020-08-10 23:10:58 -0600

    update for dataset change to v10

--- R/calculate_price_change.R
+++ R/calculate_price_change.R
@@ -5,19 +5,19 @@
 calculate_price_change <- function(data, hours_later){ #would be better to take name/unique identifier from user too here
 
   # make pkey for join
-  data$pkey <- paste0(data$pk_dummy, data$name)
+  data$pkey <- paste0(data$pkDummy, data$name)
 
   # new object for join offset
   data_join <- data
 
   # adjust by x hours
-  data_join$date_time_colorado_mst <- data_join$date_time_colorado_mst - lubridate::hours(hours_later)
+  data_join$date_time_utc <- data_join$date_time_utc - lubridate::hours(hours_later)
 
-  # make adjusted pk_dummy
-  data_join$pk_dummy <- substr(data_join$date_time_colorado_mst, 1, 13)
+  # make adjusted pkDummy
+  data_join$pkDummy <- substr(data_join$date_time_utc, 1, 13)
 
   # make adjusted pkey
-  data_join$pkey <- paste0(data_join$pk_dummy, data_join$name)
+  data_join$pkey <- paste0(data_join$pkDummy, data_join$name)
 
   # rename new offset price_usd field (only sell price needed)
   data_join <- rename(data_join, sell_price_usd_24h_later = 'sell_price_high_bid') # ADJUST FIELD NAME WITH TIDY EVAL
@@ -34,6 +34,4 @@ calculate_price_change <- function(data, hours_later){ #would be better to take 
   # unique data and return the result
   return(dplyr::distinct(data_join, pkey, .keep_all = T))
 }
-
 # Note 04/21: Remember to create new tests
-
